85c6fd44c30ed48ed058335b492e6b382e540ca4
Вольфрам <kiryushinrus@gmail.com.com>
2025-10-24 01:26:56 +0300

    Обнова5

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.expand();
 const user = tg.initDataUnsafe?.user || {};
 
-const apiUrl = "https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbwWmwLqR41d65vdpPGgQu40-FZruPeB2jmYNiZfMeiimUoQ8_xYfMAg78eXFtw_KUYx/exec";
+const apiUrl = "https://script.google.com/macros/s/AKfycbwWmwLqR41d65vdpPGgQu40-FZruPeB2jmYNiZfMeiimUoQ8_xYfMAg78eXFtw_KUYx/exec";
 
 // Привязка цветов к userId
 const userColors = {
